286605f492e38c77cf825e3b83138cfdbb67e68b
TACO2112 <45360167+Taco2112@users.noreply.github.com>
2020-02-13 08:33:17 -0500

    I AM GOING SHOPPING
    This is my to-do list , I hope I don't forget my money

--- Lab12bst.java
+++ Lab12bst.java
@@ -32,3 +32,16 @@ class Deck
 		cards = new ArrayList<Card>();
 	}    
  }
+// TO DO LIST
+/*
+in this lab i am going to have to complete the follwoing
+
+-complete the constructor so that all 52 cards of a normal card deck are assigned to the cards array
+- re-define the toString method for the Deck class so that it can be used to display the attribute values in a convenient manner
+      Okay thats pretty much it
+      THATS ALL YOU HAVE TO DO :D two things isn't that great.
+      but wait...
+  Make sure to take advantage of the toString method that already exists in the Card class
+  YOU DONT HAVE TO BUT IT'S A GOOD THING TO DO :D
+*/
+//now lets get a job :D AHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH 
